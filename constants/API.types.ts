730b84551c798bcae50c466f13956632c715c138
Narek Musakhanyan <nmusakhanyan@gmail.com>
2022-01-12 00:06:47 +0400

    add API response types

---
+++ constants/API.types.ts
@@ -0,0 +1,46 @@
+export interface MovieType {
+    Title: string;
+    Year: string;
+    imdbID: string;
+    Type: string;
+    Poster: string;
+}
+
+export interface MoviesType {
+    Search: MovieType[];
+    totalResults: string;
+    Response: string;
+}
+
+export interface RatingType {
+    Source: string;
+    Value: string;
+}
+
+export interface MovieDetailsType {
+    Title: string;
+    Year: string;
+    Rated: string;
+    Released: string;
+    Runtime: string;
+    Genre: string;
+    Director: string;
+    Writer: string;
+    Actors: string;
+    Plot: string;
+    Language: string;
+    Country: string;
+    Awards: string;
+    Poster: string;
+    Ratings: RatingType[];
+    Metascore: string;
+    imdbRating: string;
+    imdbVotes: string;
+    imdbID: string;
+    Type: string;
+    DVD: string;
+    BoxOffice: string;
+    Production: string;
+    Website: string;
+    Response: string;
+}
