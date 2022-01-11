2dd1260c39f00cc8a101524436dbcd517fdd8abf
Narek Musakhanyan <nmusakhanyan@gmail.com>
2022-01-12 00:21:52 +0400

    apply Movies context

--- App.tsx
+++ App.tsx
@@ -1,22 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {StatusBar} from 'expo-status-bar'
+import {SafeAreaView} from 'react-native-safe-area-context'
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from './hooks/useCachedResources'
+import useColorScheme from './hooks/useColorScheme'
+import Navigation from './navigation'
+import {MoviesProvider} from './contexts/MoviesContext'
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+    const isLoadingComplete = useCachedResources()
+    const colorScheme = useColorScheme()
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+    if (!isLoadingComplete) {
+        return null
+    } else {
+        return (
+            <SafeAreaView style={{flex: 1, paddingHorizontal: 10, paddingTop: 10}}>
+                <MoviesProvider>
+                    <Navigation colorScheme={colorScheme}/>
+                    <StatusBar/>
+                </MoviesProvider>
+            </SafeAreaView>
+        )
+    }
 }
